a0d007d4c1ff9104a70cc7ba1905cc9a285b9da5
vardha jain <vardhajain252gmail.com>
2018-08-30 23:11:57 +0530

    Final commit

---
+++ Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="scraping3.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #form1 {
+            height: 629px;
+            width: 1361px;
+            margin-bottom: 5px;
+        }
+    </style>
+</head>
+<body style="height: 629px; width: 1358px; margin-left: 0px; margin-bottom: 3px">
+    <form id="form1" runat="server">
+    <div style="height: 629px; width: 1351px; margin-right: 0px; margin-left: 0px;">
+        <asp:Label ID="Label6" runat="server" Text="Enter new height and width :" style="position:absolute; top: 321px; left: 46px; height: 25px; width: 403px; font-weight: 700;" Visible="False" ForeColor="White" Font-Size="Large"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server" style="position:absolute; top: 362px; left: 319px;" Visible="False"></asp:TextBox>
+        <asp:Label ID="Label3" runat="server" Text="Do you want to resize image before downloading?"  style="position:absolute; top: 188px; left: 49px; width: 405px; height: 27px; font-weight: 700;" ForeColor="White" Font-Size="Large"></asp:Label>
+        <asp:ListBox ID="ListBox1" runat="server" style="position:absolute; top: 254px; left: 742px; width: 372px; height: 232px;"></asp:ListBox>
+    
+        <asp:Button ID="Button1" runat="server" Text="Download" style="position:absolute; top: 508px; left: 1024px; width: 159px; right: 148px; height: 32px;" OnClick="Button1_Click" ForeColor="Black"/>
+    
+        <asp:Button ID="Button2" runat="server" Text="Stop" style="position:absolute; top: 586px; left: 904px; width: 150px; height: 30px; margin-bottom: 0px;" OnClick="Button2_Click" />
+    
+    
+        <asp:Label ID="Label2" runat="server" Text="Select category and click download to download the news:"   style="position:absolute; top: 203px; left: 727px; width: 417px; height: 24px; font-weight: 700;" ForeColor="White" Font-Size="Large"></asp:Label>
+    
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server"  style="position:absolute; top: 214px; left: 50px; width: 28px; height: 43px;" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged" ForeColor="White">
+            <asp:ListItem>Yes</asp:ListItem>
+            <asp:ListItem>No</asp:ListItem>
+        </asp:RadioButtonList>
+    
+        <asp:TextBox ID="TextBox1" runat="server" style="position:absolute; top: 361px; left: 103px; right: 688px;" Visible="False"></asp:TextBox>
+    
+        <asp:Label ID="Label4" runat="server" Text="Height" style="position:absolute; top: 365px; left: 49px; height: 19px; width: 51px;" Visible="False" ForeColor="White"></asp:Label>
+        <asp:Label ID="Label5" runat="server" Text="Width" style="position:absolute; top: 364px; left: 278px; height: 23px; width: 41px; right: 780px;" Visible="False" ForeColor="White"></asp:Label>
+    
+        <asp:Label ID="Label7" runat="server" style="position:absolute; top: 39px; left: 357px; width: 556px;" Text=" News Downloader" Font-Size="50pt" ForeColor="White"></asp:Label>
+    
+        <asp:TextBox ID="TextBox3"  runat="server" style="position:absolute; top: 508px; left: 801px; width: 190px; height: 25px;"></asp:TextBox>
+    
+        <asp:Label ID="Label8" runat="server" Text="ENTER PATH:" style="position:absolute; top: 515px; left: 668px; width: 112px; height: 24px; font-weight: 700;" ForeColor="White"></asp:Label>
+    
+        <asp:Image ID="Image1" runat="server"
+            style=" top: 15px; left: 10px; margin-top: 0px; margin-bottom: 0px; font-weight: 700; font-size: xx-large;" Height="630px" Width="1355px" ForeColor="White" ImageUrl="~/images/Abstract-Purple-Background-Wallpaper-HD-1152x720.jpg" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,428 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+
+namespace scraping3
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+
+        H2PTesting.h2ptrans h2p;
+        public static int z,w,h;
+        public static String str;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            h2p = new H2PTesting.h2ptrans();
+             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            HtmlWeb hw = new HtmlWeb();
+            String url = "https://www.jagran.com";
+            try
+            {
+                doc = hw.Load(url);
+                HtmlNodeCollection node = doc.DocumentNode.SelectNodes("//*[contains(@class,'container')]//h3/a[@href]");
+                if (ListBox1.Items.Count<=0)
+                {
+                    foreach (HtmlNode nodes in node)
+                    {
+                        HtmlAttribute att = nodes.Attributes["href"];
+                        ListItem dataItem = new ListItem();
+                        dataItem.Text = nodes.InnerText;
+                        dataItem.Value = att.Value;
+                        ListBox1.Items.Add(dataItem);
+                    }
+                }
+            }
+            catch(Exception a)
+            {
+               
+                Response.Write(@"<script language='javascript'>alert('Check your Internet Connection and Try Again');</script>");
+            }
+        }
+
+        public void link3(String url,String hreftag)
+        {
+            HtmlAgilityPack.HtmlDocument doc2 = new HtmlAgilityPack.HtmlDocument();
+            HtmlWeb hw2 = new HtmlWeb();
+            String newurl1 = "https://" + url + hreftag;
+            
+            try
+            {
+                doc2 = hw2.Load(newurl1);
+               
+                if ("//*[contains(@class,'bodysummery')]//figcaption" != null && "//*[contains(@class,'bodysummery')]//img" != null && doc2.DocumentNode.SelectSingleNode("//*[@id=\"jagran_image_id\"]").GetAttributeValue("src", string.Empty)!="//www.jagranimages.com/images/jagran_logo.jpg")
+                {
+                   
+                        z++;
+
+                        HtmlNodeCollection nodes = doc2.DocumentNode.SelectNodes("//*[contains(@class,'articleBody')]//p");
+                        String s = null;
+                        if (nodes != null)
+                        {
+                            foreach (HtmlNode node in nodes)
+                            {
+                                s += node.InnerText;
+
+                            }
+                            String abc = h2p.translate(s);
+                            System.IO.File.WriteAllText(TextBox3.Text + "Top-Level Folder\\news\\" + z, abc);
+                        }
+
+                        var pre = doc2.DocumentNode.Descendants("figure").FirstOrDefault();
+                        var links = pre.Descendants("img");
+                        var links1 = pre.Descendants("figcaption");
+                        foreach (var node in links1)
+                        {
+                            String abc1 = h2p.translate(node.InnerText);
+                            System.IO.File.WriteAllText(TextBox3.Text + "Top-Level Folder\\captions\\" + z, abc1);
+
+                        }
+
+
+                        foreach (var node in links)
+                        {
+
+
+                            string a = "http:" + node.GetAttributeValue("src", string.Empty);
+                            string n = node.GetAttributeValue("alt", string.Empty);
+                            String abc2 = h2p.translate(n);
+                            System.IO.File.WriteAllText(TextBox3.Text + "Top-Level Folder\\headings\\" + z, abc2);
+                            string saveLocation = TextBox3.Text + "Top-Level Folder\\images\\" + z + ".jpg";
+
+                            byte[] imageBytes;
+                            HttpWebRequest imageRequest = (HttpWebRequest)WebRequest.Create(a);
+                            WebResponse imageResponse = imageRequest.GetResponse();
+
+                            Stream responseStream = imageResponse.GetResponseStream();
+
+                            using (BinaryReader br = new BinaryReader(responseStream))
+                            {
+                                imageBytes = br.ReadBytes(500000);
+                                br.Close();
+                            }
+                            responseStream.Close();
+                            imageResponse.Close();
+
+                            FileStream fs = new FileStream(saveLocation, FileMode.Create);
+                            BinaryWriter bw = new BinaryWriter(fs);
+                            if (RadioButtonList1.SelectedIndex == 0)
+                            {
+                                h = Convert.ToInt16(TextBox1.Text);
+                                w = Convert.ToInt16(TextBox2.Text);
+                                System.Drawing.Image i = byteArrayToImage(imageBytes);
+                                System.Drawing.Image i2 = ReSize(i, h, w);
+                                byte[] i3 = imageToByteArray(i2);
+                                try
+                                {
+                                    bw.Write(i3);
+                                }
+                                finally
+                                {
+                                    fs.Close();
+                                    bw.Close();
+                                }
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    bw.Write(imageBytes);
+                                }
+                                finally
+                                {
+                                    fs.Close();
+                                    bw.Close();
+                                }
+                            }
+                        }
+                }
+            }
+            catch
+            {
+                 Response.Write(@"<script language='javascript'>alert('Check your Internet Connection and Try Again');</script>");
+            
+            }
+            
+        }
+        public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+            return ms.ToArray();
+        }
+        public System.Drawing.Image byteArrayToImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            System.Drawing.Image returnImage = System.Drawing.Image.FromStream(ms);
+            return returnImage;
+        }
+
+        static System.Drawing.Image ReSize(System.Drawing.Image imgPhoto, int Width, int Height)
+        {
+           int sourceWidth = imgPhoto.Width;
+           int sourceHeight = imgPhoto.Height;
+           
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)Width / (float)sourceWidth);
+            nPercentH = ((float)Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+        
+            else
+            {
+                nPercent = nPercentW;
+           
+            }
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g =Graphics.FromImage((System.Drawing.Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imgPhoto, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (System.Drawing.Image)b;
+            
+        }
+    
+        public void link4(String url,String hreftag)
+        {
+            HtmlAgilityPack.HtmlDocument doc1 = new HtmlAgilityPack.HtmlDocument();
+            HtmlWeb hw1 = new HtmlWeb();
+           
+            String newurl = "https://" + url + hreftag;
+            try
+            {
+                if (hreftag.Contains("https"))
+                {
+                    doc1 = hw1.Load(hreftag);
+                }
+
+                else
+                {
+                    doc1 = hw1.Load(newurl);
+                }
+                if (doc1.DocumentNode.SelectNodes("*//*[contains(@class,'pagination border0')]") == null)
+                {
+                    str = "";
+                    foreach (HtmlNode node in doc1.DocumentNode.SelectNodes("*//*[contains(@class,'MainHd')]//a[@href]"))
+                    {
+                        if (str != node.Attributes["href"].Value)
+                        {
+                             HtmlAttribute att1 = node.Attributes["href"];
+                             link4(url, att1.Value);
+                        }
+                        str = node.Attributes["href"].Value;
+                        
+                    }
+                }
+                else
+                {
+                    while (true)
+                    {
+                        if (doc1.DocumentNode.SelectNodes("*//*[contains(@class,'last')]") == null)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            foreach (HtmlNode node in doc1.DocumentNode.SelectNodes("*//*[contains(@class,'last')]//a[@href]"))
+                            {
+
+                                HtmlAttribute att1 = node.Attributes["href"];
+                                String str = "https://www.jagran.com" + att1.Value;
+                                // doc1 = hw1.Load(str);
+                                foreach (HtmlNode link in doc1.DocumentNode.SelectNodes("//*[contains(@class,'topicList')]//a[@href]"))
+                                {
+                                    HtmlAttribute att = link.Attributes["href"];
+                                    
+                                    link3("www.jagran.com", att.Value);
+                                }
+                                doc1 = hw1.Load("https://www.jagran.com" + att1.Value);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception c)
+            {
+                 Response.Write(@"<script language='javascript'>alert('Check your Internet Connection and Try Again');</script>");
+            
+            }
+         
+        }
+
+       
+
+
+        public void link2(String url, String hreftag)
+        {
+            
+            HtmlAgilityPack.HtmlDocument doc1 = new HtmlAgilityPack.HtmlDocument();
+            HtmlWeb hw1 = new HtmlWeb();
+           
+            String newurl = "https://" + url + hreftag;
+            try
+            {
+                doc1 = hw1.Load(newurl);
+                foreach (HtmlNode node in doc1.DocumentNode.SelectNodes("*//*[contains(@class,'morebt')]/a[@href]"))
+                {
+                    HtmlAttribute att1 = node.Attributes["href"];
+                    String str = "https://www.jagran.com" + att1.Value;
+                    link1(url, att1.Value);
+
+                }
+            }
+            catch(Exception b)
+            {
+             Response.Write(@"<script language='javascript'>alert('Check your Internet Connection and Try Again');</script>");
+            }
+
+          
+        
+        }
+        public void link1(String url,String hreftag)
+        {
+            z++;
+           
+            HtmlAgilityPack.HtmlDocument doc1 = new HtmlAgilityPack.HtmlDocument();
+            HtmlWeb hw1 = new HtmlWeb();
+            String str = "https://www.jagran.com" + hreftag;
+            try
+            {
+                doc1 = hw1.Load(str);
+                HtmlNodeCollection nodes = doc1.DocumentNode.SelectNodes("//*[contains(@class,'textbx')]//p");
+                String s = null;
+                if (nodes != null)
+                {
+                    foreach (HtmlNode node in nodes)
+                    {
+                        s += node.InnerText;
+
+                    }
+                    System.IO.File.WriteAllText(@"o:\Top-Level Folder\news\" + z, s);
+                }
+                foreach (var node in doc1.DocumentNode.SelectNodes("*//*[contains(@class,'imgbx')]//img"))
+                {
+
+                    System.IO.File.WriteAllText(@"o:\Top-Level Folder\headings\" + z, node.GetAttributeValue("alt", string.Empty));
+
+                    string a = "http:" + node.GetAttributeValue("src", string.Empty);
+                    string saveLocation = @"o:\Top-Level Folder\images\" + z + ".jpg";
+
+                    byte[] imageBytes;
+                    HttpWebRequest imageRequest = (HttpWebRequest)WebRequest.Create(a);
+                    WebResponse imageResponse = imageRequest.GetResponse();
+
+                    Stream responseStream = imageResponse.GetResponseStream();
+
+                    using (BinaryReader br = new BinaryReader(responseStream))
+                    {
+                        imageBytes = br.ReadBytes(500000);
+                        br.Close();
+                    }
+                    responseStream.Close();
+                    imageResponse.Close();
+
+                    FileStream fs = new FileStream(saveLocation, FileMode.Create);
+                    BinaryWriter bw = new BinaryWriter(fs);
+                    try
+                    {
+                        bw.Write(imageBytes);
+                    }
+                    finally
+                    {
+                        fs.Close();
+                        bw.Close();
+                    }
+                }
+            }
+            catch(Exception a)
+            {
+                 Response.Write(@"<script language='javascript'>alert('Check your Internet Connection and Try Again');</script>");
+            }
+            
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string rootpath = TextBox3.Text;
+            string folderName =  TextBox3.Text+"Top-Level Folder";
+            string pathString = System.IO.Path.Combine(folderName, "images");
+            string pathString2 = TextBox3.Text+"Top-Level Folder\\headings";
+            string pathString3 = TextBox3.Text + "Top-Level Folder\\news";
+            string pathString4 = TextBox3.Text + "Top-Level Folder\\captions";
+            System.IO.Directory.CreateDirectory(pathString);
+            string fileName = System.IO.Path.GetRandomFileName();
+            if (!System.IO.Directory.Exists(pathString))
+            {
+                System.IO.Directory.CreateDirectory(pathString);
+            }
+            if (!System.IO.Directory.Exists(pathString2))
+            {
+                System.IO.Directory.CreateDirectory(pathString2);
+            }
+            if (!System.IO.Directory.Exists(pathString3))
+            {
+                System.IO.Directory.CreateDirectory(pathString3);
+            }
+            if (!System.IO.Directory.Exists(pathString4))
+            {
+                System.IO.Directory.CreateDirectory(pathString4);
+            }
+
+             String url = "www.jagran.com";
+             String str = ListBox1.SelectedItem.Value;
+             if (ListBox1.SelectedItem.Value == "/spiritual/rashifal-hindi.html")
+             {
+                 link2(url, str);
+             }
+             else
+             {
+                 link4(url, str);
+             }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(RadioButtonList1.SelectedIndex==0)
+            {
+                TextBox1.Visible = true;
+                TextBox2.Visible = true;
+                Label4.Visible = true;
+                Label5.Visible = true;
+                Label6.Visible = true;
+            }
+        }
+   
+
+    }
+}
